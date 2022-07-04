f9bafd2f371fac159a9ba23c9d2e1dff73c87634
Raneru <107743292+Raneru2004@users.noreply.github.com>
2022-07-04 12:13:38 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function onChange(event){
+async function onChange(event){
     const ans=event.target.value;
     console.log(ans)
-    document.write(renderMovies(ans));
+    localStorage.setItem("pass",ans);
+    //document.write(renderMovies(ans));
+    //console.log(renderMovies(ans));
 }
+//onclick="window.location.href='http://127.0.0.1:5500/findyourmovie.html';"  use this on search button if needed
