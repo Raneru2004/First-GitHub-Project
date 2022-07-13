56141722131a39724f1f356f5c5d8b3bfbdc259d
Raneru <107743292+Raneru2004@users.noreply.github.com>
2022-07-13 21:05:16 +1000

    Add files via upload

--- Header.js
+++ Header.js
@@ -1,14 +1,57 @@
-import React from "react"
-import { ReactDOM } from "react"
+import React from 'react';
+import './Header.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from '../features/counter/userSlice';
+import { auth } from '../firebase';
 
 function Header(){
-    return(
-        <header>
-            <img className="img__meme" src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"/>
-            <p>Meme Generator</p>
-        </header>
-        
-    )
+  const user =useSelector(selectUser)
+  const dispatch=useDispatch();
+
+  const signOut=()=>{
+    auth.signOut().then(()=>{
+      dispatch(logout())
+    })
+
+  }
+
+
+    return (
+        <div className='header'>
+            <div className='header__left'>
+              <IconButton>
+                <MenuIcon/>
+              </IconButton>
+               <img src="https://i.pinimg.com/564x/ae/47/fa/ae47fa9a8fd263aa364018517020552d.jpg" alt=""/>
+            </div>
+
+            <div className='header__middle'>
+              <SearchIcon className='search__icon'/>
+              <input placeholder='Search Mail' type='text'/>
+              <ArrowDropDownIcon className='header__input--caret'/>
+            </div>
+
+            <div className='header__right'>
+               <IconButton>
+                <AppsIcon/>
+               </IconButton>
+               <IconButton>
+                <NotificationsIcon/>
+               </IconButton>
+               <IconButton>
+                <AccountCircleIcon onClick={signOut} src={user?.photoUrl}/>
+               </IconButton>
+            </div>
+
+        </div>
+    );
 }
 
-export default Header
+export default Header;
