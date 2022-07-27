7a94017cbe5d3830405aebbd0a7397532097e9b1
Raneru <107743292+Raneru2004@users.noreply.github.com>
2022-07-27 20:21:28 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+import logo from './logo.svg';
 import './App.css';
-import Sidebar from './components/Sidebar';
-import React from 'react';
-import Feed from './components/Feed';
-import Widgets from './components/Widgets';
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+import { Provider } from 'react-redux';
+import { Component } from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import store from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <Sidebar/>
 
-      <Feed/>
+class App extends Component {
 
-      <Widgets/>
-    </div>
-  );
+// WE ARE GONNA BASICALLY ADD A POST TO THE POSTS THAT ARE DISPLAYED USING REDUX AND FETCH FROM POST ACTION
+
+  render(){
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            > <Postform/>
+            <hr/>
+              <Posts/>
+            </a>
+          </header>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
